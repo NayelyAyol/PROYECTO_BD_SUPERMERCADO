4984b82a208064b529cbc13e628338c391d5bea5
NayelyAyol <nayely.ayol@epn.edu.ec>
2025-08-03 20:24:46 -0500

    Protección de datos

---
+++ 11_Proteccion_Gestion_Critica.sql
@@ -0,0 +1,39 @@
+use Supermercado;
+go
+
+-- Protección de Datos y Gestión Crítica
+-- Simulación de anonimización y enmascaramiento.
+CREATE FUNCTION enmascarar_email (@email NVARCHAR(100))
+RETURNS NVARCHAR(100)
+AS
+BEGIN
+    DECLARE @arroba INT = CHARINDEX('@', @email)
+    IF @arroba <= 3
+        RETURN 'Email inválido'
+
+    RETURN LEFT(@email, 3) + '***' + SUBSTRING(@email, @arroba, LEN(@email))
+END;
+GO
+
+
+SELECT nombre, dbo.enmascarar_email(email) AS email_mask
+FROM cliente;
+GO
+
+CREATE FUNCTION enmascarar_cedula (
+    @cedula NVARCHAR(10)
+)
+RETURNS NVARCHAR(10)
+AS
+BEGIN
+    IF LEN(@cedula) != 10
+        RETURN 'Cédula inválida'
+
+    RETURN LEFT(@cedula, 2) + '*****' + RIGHT(@cedula, 3)
+END
+GO
+
+-- Prueba del Enmascaramiento de la cedula
+SELECT nombre, dbo.enmascarar_cedula(cedula) AS cedula_mask
+FROM cliente;
+GO
